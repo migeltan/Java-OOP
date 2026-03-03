0083be31c725277c729610c3501e856afdc22f07
miggy <migellltan@gmail.com>
2026-03-03 08:06:53 +0800

    act 2: java program using assignment operators

---
+++ School/HW_A2P1/New_A2P1N3.java
@@ -0,0 +1,23 @@
+//package HW_A2P1;
+
+//Create a java program using assignment operators
+//produces: Addition: 120, Subtraction: 70, Multiplication: 144, Division: 16
+
+public class New_A2P1N3 {
+    public static void main(String[] args) {
+        int add = 100;
+        add += 20;
+        int sub = 100;
+        sub -= 30;
+        int mul = 12;
+        mul *= 12;
+
+        int div = 64;
+        div /= 4;
+
+        System.out.println("Addition: " + add);
+        System.out.println("Subtraction: " + sub);
+        System.out.println("Multiplication: " + mul);
+        System.out.println("Division: " + div);
+    }
+}
