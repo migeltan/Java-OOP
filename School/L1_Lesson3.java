35c762a3dc6edb16a4060dee1363127e5651c6f2
miggy <migellltan@gmail.com>
2026-03-07 00:52:30 +0800

    lesson 3: bitwise operators

---
+++ School/L1_Lesson3.java
@@ -0,0 +1,18 @@
+public class L1_Lesson3 {
+    
+    public static void main(String[] args){
+        //int a = 2;
+        //int b = 5;
+
+       // int min (a<5) ? a : b;
+        //System.out.println(min);
+
+        //BITWISE & and |
+        int a = 5;
+        int b = 3;
+        System.out.println(a & b);
+        System.out.println(a | b);
+
+    }
+
+}
